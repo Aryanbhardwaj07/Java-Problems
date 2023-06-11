f0ffa918d1a13f48c2b51bf10cc1c54c0f05b570
AnshulRohilla45 <anshulrohilla45@gmail.com>
2023-06-11 23:42:27 +0530

    added

---
+++ month.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class month {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int button = sc.nextInt();
+
+        switch (button) {
+            case 1:
+                System.out.println("jan");
+                break;
+            case 2:
+                System.out.println("feb");
+                break;
+            case 3:
+                System.out.println("march");
+                break;
+        }
+
+    }
+
+}
