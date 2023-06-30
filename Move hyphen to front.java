e32e91f7a36f990d9cc3979b6b4b50c26b2a7260
ARYAN BHARDWAJ <80642649+Aryanbhardwaj07@users.noreply.github.com>
2023-06-30 10:02:34 +0530

    Update Move hyphen to front.java

--- Move hyphen to front.java
+++ Move hyphen to front.java
@@ -17,3 +17,30 @@ public class shiftstring {
         System.out.println(String.valueOf(str));
     }
 }
+//*****************Second Approach************
+public class binary_String {
+    public static void stringArrangement(String str){
+        int count = 0;
+        String res = "";
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(!Character.isLetter(ch)){
+                count++;
+            }
+        }
+        for(int i=0; i<count; i++){
+            res+='_';
+        }
+        for(int i=0; i<str.length(); i++){
+            if(Character.isLetter(str.charAt(i))){
+                res+=str.charAt(i);
+            }
+        }
+        System.out.println(res);
+    }
+    public static void main(String[] args) {
+        String str = "He-llo-Wo-rl-d";
+
+        stringArrangement(str);
+    }
+}
